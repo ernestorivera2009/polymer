5428e6a53023fd6b469f53641b15d3519c35a3b1
Levent Gurses <levent.gurses@64clicks.com>
2014-12-28 20:49:26 -0500

    API keys

--- public/js/keys.js
+++
@@ -1,5 +0,0 @@
-var IMPORT_IO_KEYS = {
-    userGuid	: "6ee96827-08d2-46ef-8004-f075883e6a8e",
-	apiKey  	: "t0UBOvUUT2PQVMx8tLvNup/2dgnSeOYVm85sOkHQ0jKDmoLNvh/IKMgwPGlc3rEua5KEFUdKRD8vaPDkfMAUPg==",
-	sourceGuid 	: "b1ebfba9-e82f-45ed-b291-2faca7921a84"
-};
